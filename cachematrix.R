1ec7aee659bd046487f641c2362ea09bf9d0595a
TZGrove <tijana.grove@gmail.com>
2023-11-18 11:14:18 -0500

    Week 3 coding assignment (Coding Assignment #2)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The ovearll goal of makeCacheMatrix function is to cash inverse of a square
+## matrix so that it can be looked up in the cache rather than repeatedly 
+## computed.
 
-## Write a short comment describing this function
+## This functio creates a special matrix object that can store (cashe) matrix
+## and it's inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+      makeCacheMatrix <- function(x = matrix())  {
+                  m <- NULL
+                  set <- function(y) {
+                          x <<- y
+                          m <<- NULL
+  }
+                  
+                  get <- function() x
+          setm_inverse<- function(m_inverse) m <<- m_inverse
+          getm_inverse <- function() m
+          list(set = set, get = get,
+          setm_inverse = setm_inverse,
+          getm_inverse = getm_inverse)
+  
 }
 
+## This fuction calculates inverse of the special matrix object created with the
+## makeCasheMatrix fuction. However, it first checks if inverse is already 
+## calculated and if so, returns cashed data (with the message). If not, 
+## it calculates the new inverse.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+      cacheSolve <- function(x, ...) {
+          m <= x$getm_inverse()
+          if(!is.null(m_inverse)) {
+          message("getting cached data")
+          return(m)
+    }
+    
+          mat <- x$get()
+          invrs <- solve(mat, ...)
+          x$setinverse(invrs)
+          invrs
+  }
